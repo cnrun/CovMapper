d5c05cff1c5337f4c3c15dcabfec50d9c3c7da30
cfdd73 <eric@louvard.de>
2020-10-18 16:12:56 +0200

    Change host for tests

--- tests/testcafe/introScreens.spec.ts
+++ tests/testcafe/introScreens.spec.ts
@@ -1,6 +1,6 @@
 import { Selector } from "testcafe";
 
-fixture`Intro screens test`.page`http://test.covmap.de`;
+fixture`Intro screens test`.page`http://localhost:8080`;
 
 test("Finish intro screens", async (t) => {
   const nextButton = Selector("a").withAttribute("role", "button").child().withText("Weiter");
@@ -11,13 +11,10 @@ test("Finish intro screens", async (t) => {
   const titleForIntro3 = "Was zeigt mir die CovMap an?";
   const titleTextForPostalCode = "Für Dein regionales Risiko brauchen wir noch die Postleitzahl Deines Wohnortes";
 
-  await t.expect(websiteText).contains(titleForIntro1).click(nextButton);
-
+  await t.expect(websiteText).contains("Lade Daten");
+  await t.wait(20000).expect(websiteText).contains(titleForIntro1).click(nextButton);
   await t.expect(websiteText).contains(titleForIntro2).click(nextButton);
-
   await t.expect(websiteText).contains(titleForIntro3).click(nextButton);
-
   await t.expect(websiteText).contains(titleTextForPostalCode).pressKey("1 2 3 4 5 enter");
-
   await t.expect(websiteText).notContains(titleTextForPostalCode);
 });
